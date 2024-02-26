5f32ee735e9336396f76db11878630d47527c57f
Almat <72481308+Almat-prog@users.noreply.github.com>
2024-02-26 09:39:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def apply_callback(callback, lst):
-    return [callback(item) for item in lst]
-
-numbers = [1, 2, 3, 4, 5]
-
-mapped = map(lambda x: x * 2, numbers)
-
-result = apply_callback(lambda x: x * 2, numbers)
-
-print(result)
+numbers = (1, 2, 3, 4, 5, -1, -2, -3, -4, -5)
+even_numbers = filter(lambda x: x > 0,numbers)
+squared_numbers = map(lambda x: x * x, even_numbers)
+print(list(squared_numbers))
